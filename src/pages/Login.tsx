3ccdb3772fa2da676fae36e8ae834939aade3ff5
dylanR96 <dylan.ravenson@gmail.com>
2024-09-06 21:07:51 +0200

    Added form data, and styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ interface LoginForm {
   password: string;
 }
 
-const Login = (props: Props) => {
+const Login = () => {
   const [formData, setFormData] = useState<LoginForm>({
     username: "",
     password: "",
@@ -25,31 +25,35 @@ const Login = (props: Props) => {
   };
   return (
     <>
-      <div>
-        <Frame />
-        <h2>Where It's @</h2>
-        <h2>STAFF</h2>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            id="userN"
-            onChange={handleChange}
-            value={formData.username}
-            placeholder="Username"
-          />
-          <input
-            type="text"
-            name="password"
-            id="pass"
-            onChange={handleChange}
-            value={formData.password}
-            placeholder="Password"
-          />
-          <button>Login in</button>
-        </form>
+      <div className="main">
+        <div className="main__logo">
+          <Frame />
+          <h2>Where It's @</h2>
+          <h2>STAFF</h2>
+        </div>
+        <div>
+          <form className="main__form" onSubmit={handleSubmit}>
+            <input
+              className="main__form-inputs"
+              type="text"
+              name="username"
+              id="userN"
+              onChange={handleChange}
+              value={formData.username}
+              placeholder="Username"
+            />
+            <input
+              className="main__form-inputs"
+              type="text"
+              name="password"
+              id="pass"
+              onChange={handleChange}
+              value={formData.password}
+              placeholder="Password"
+            />
+            <button className="main__form-btn">Login in</button>
+          </form>
+        </div>
       </div>
     </>
   );
